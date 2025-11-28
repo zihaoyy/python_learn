dd5cf9daf842bdc45c0512034850126a5f72c713
Zihaoyy <z@fv.cx>
2025-11-28 15:18:06 +0800

    feat: 注释与变量

---
+++ 03-变量.py
@@ -0,0 +1,23 @@
+"""
+变量
+    定义格式： 变量名 = 变量值
+"""
+
+a = 100
+print(a)
+
+a = 200
+print(a)
+
+"""
+变量类型
+    int 整形: 12
+    float 浮点型: 1.22 -0.2
+    bool 布尔型: True/False
+    string 字符串: '1' "1" （多行字符串，以三引号形式）
+"""
+
+# 查看类型 type(var)
+b = False
+print(b)
+print(type(b))
